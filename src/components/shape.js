1b1018fa494c1507f4e65ce7da0598d8287259b4
manojbhandi <97438009+manojbhandi@users.noreply.github.com>
2022-07-11 16:10:02 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,19 @@
 // declare class
 
 // export class using module.exports
+
+class Shape{
+
+
+    color = blue; 
+
+    drawShape(){
+
+    }
+
+    calculateArea(){
+
+    }
+}
+
+module.export = Shape;
